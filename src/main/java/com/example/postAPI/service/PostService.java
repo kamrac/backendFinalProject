9c0d78d45e9a9e3b148204f698e96e62853a8236
Kamra <kamra.c@gmail.com>
2022-12-12 22:19:10 +0000

    changes to task 9

--- src/main/java/com/example/postAPI/service/PostService.java
+++ src/main/java/com/example/postAPI/service/PostService.java
@@ -3,10 +3,14 @@ package com.example.postAPI.service;
 import com.example.postAPI.repository.PostRepository;
 import com.example.postAPI.repository.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class PostService {
+public class PostService{
     private final PostRepository postRepository;
 
     public PostService(@Autowired PostRepository postRepository) {
@@ -16,15 +20,23 @@ public class PostService {
 
     public Post save(Post post) {
         //TODO implement this method
-        return null;
+        return postRepository.save(post);
     }
 
     public void delete(int postId) {
         //TODO implement this method
+        postRepository.deleteById(postId);
+    }
+
+    public List<Post> all()
+    {
+        //TODO implement this method
+        return postRepository.findAll();
     }
 
     public Post findById(int postId) {
         //TODO implement this method
-        return null;
+        Optional<Post> post = postRepository.findById(postId);
+        return post.get();
     }
 }
