d34b669955e5d8d6f4a8416fa07a23be54b036f3
Your Name <you@example.com>
2022-12-12 15:19:42 -0600

    Item added

--- src/main/java/com/example/postAPI/repository/PostRepository.java
+++ src/main/java/com/example/postAPI/repository/PostRepository.java
@@ -3,9 +3,12 @@ package com.example.postAPI.repository;
 
 import com.example.postAPI.repository.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called itemRepository
 // CRUD refers Create, Read, Update, Delete
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
+@Repository
+public interface PostRepository extends JpaRepository<Post,Integer> {
 }
+
--- src/main/java/com/example/postAPI/service/PostService.java
+++ src/main/java/com/example/postAPI/service/PostService.java
@@ -3,7 +3,11 @@ package com.example.postAPI.service;
 import com.example.postAPI.repository.PostRepository;
 import com.example.postAPI.repository.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -16,15 +20,24 @@ public class PostService {
 
     public Post save(Post post) {
         //TODO implement this method
-        return null;
+        return postRepository.save(post);
     }
 
     public void delete(int postId) {
         //TODO implement this method
+        postRepository.deleteById(postId);
+    }
+
+    public List<Post> all()
+    {
+        //TODO implement this method
+        return postRepository.findAll();
     }
 
     public Post findById(int postId) {
         //TODO implement this method
-        return null;
+        Optional<Post> post = postRepository.findById(postId);
+        return post.get();
     }
+
 }
